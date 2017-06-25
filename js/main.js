d09901b292bf54d0677419b77819a6be1a956955
khunemz <khunemz@gmail.com>
2017-06-25 12:49:25 +0700

    init project

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+require.config({
+    paths: {
+        'jquery': 'js/vendor/jquery-1.11.2.min' , 
+        'underscore': 'js/vendor/underscore' , 
+        'backbone': 'js/vendor/backbone' , 
+        'bootstrap': 'js/vendor/bootstrap'
+    } , 
+    // deps = dependencies
+    shim: {
+        'underscore': {
+            exports: '_' 
+        }, 
+        'backbone': {
+            deps:[
+                'underscore' , 
+                'jquery'
+            ] , 
+            exports: 'Backbone'
+        },
+        'bootstrap': {
+            deps: [
+                'jquery'
+            ]
+        }
+    }
+});
+
+require([
+    'backbone' 
+] , function (Backbone) {
+    'use strict';
+
+    console.log(Backbone);
+
+});
